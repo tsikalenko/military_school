56aaadf74dbe891f409b987accee4e24744d0f65
Никита Цикаленко <nikita.tsikalenko@gmail.com>
2022-10-07 21:38:31 +0300

    create services page

---
+++ client/src/views/AllServices/AllServices.jsx
@@ -0,0 +1,67 @@
+import './allServices.scss'
+import {Link} from "react-router-dom";
+
+const AllServices = () => {
+    return <div className="services container">
+        <h2 className='services__title'>All Services</h2>
+
+        <div className="services__item">
+            <div className="services__photo">
+                <img src="./img/mainSlider(1).jpg" alt="service 1" className="services__img" width={280}/>
+            </div>
+            <div className="services__info">
+                <h3 className="services__subtitle">Lorem ipsum</h3>
+                <p className="services__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
+                    distinctio dolorem dolores fuga magnam magni porro qui. Amet consectetur deserunt error est ipsam
+                    perspiciatis repellendus voluptas. Architecto excepturi rem soluta</p>
+                <Link to='#' className="button button--center button--accent">More info</Link>
+            </div>
+
+
+        </div>
+
+        <div className="services__item services__item--right">
+            <div className="services__photo">
+                <img src="./img/mainSlider(2).jpg" alt="service 2" className="services__img" width={280}/>
+            </div>
+
+            <div className="services__info">
+                <h3 className="services__subtitle">Lorem ipsum</h3>
+                <p className="services__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+                    dolores eligendi illo, modi neque nobis perspiciatis quos sequi suscipit tempore. Consectetur
+                    corporis doloribus ex iusto laborum pariatur quibusdam repellat, vel.</p>
+                <Link to='#' className="button button--center button--accent">More info</Link>
+            </div>
+        </div>
+
+        <div className="services__item">
+            <div className="services__photo">
+                <img src="./img/mainSlider(3).jpg" alt="service 3" className="services__img" width={280}/>
+            </div>
+
+            <div className="services__info">
+                <h3 className="services__subtitle">Lorem ipsum</h3>
+                <p className="services__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque
+                    cupiditate fugit incidunt labore odio, suscipit unde! Amet architecto aut delectus dignissimos
+                    dolorum, ipsam, magni officiis quam quia quis temporibus!</p>
+                <Link to='#' className="button button--center button--accent">More info</Link>
+            </div>
+        </div>
+
+        <div className="services__item services__item--right">
+            <div className="services__photo">
+                <img src="./img/mainSlider(4).jpg" alt="service 4" className="services__img" width={280}/>
+            </div>
+
+            <div className="services__info">
+                <h3 className="services__subtitle">Lorem ipsum</h3>
+                <p className="services__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid
+                    blanditiis, eaque facilis impedit laudantium modi nesciunt officiis optio voluptates? Blanditiis
+                    distinctio eius facere impedit labore repellat saepe soluta sunt.</p>
+                <Link to='#' className="button button--center button--accent">More info</Link>
+            </div>
+        </div>
+    </div>
+}
+
+export default AllServices;
