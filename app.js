154b73dd286ef14cfe05846cd3a3c51c1a890d75
Никита Цикаленко <nikita.tsikalenko@gmail.com>
2022-10-08 23:12:05 +0300

    fix code style

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.DB_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
